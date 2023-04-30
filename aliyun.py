9e6cf8e81674af263706a9617b08a7155ef3487e
yjrqz777 <3210551161@qq.com>
2023-04-30 16:44:46 +0800

    整理一下

--- aliyun.py
+++ aliyun.py
@@ -1,5 +1,10 @@
 import requests
 import datetime
+import os
+import notify
+
+message = ""
+
 
 class AliyunSignIn(object):
     def __init__(self, refresh_tokens):
@@ -23,10 +28,12 @@ class AliyunSignIn(object):
         return None, None
 
     def sign_in(self):
+        messages = ''
         for refresh_token in self.refresh_tokens:
             access_token, nick_name = self.get_access_token(refresh_token)
             if access_token:
-                print(f'access_token获取完成，{access_token}欢迎{nick_name}\n开始签到')
+                print(f'access_token获取完成, {access_token}欢迎{nick_name}\n开始签到')
+                messages = messages + '\n' + f'access_token获取完成, \n欢迎{nick_name}\n开始签到'
                 url = 'https://member.aliyundrive.com/v1/activity/sign_in_list'
                 headers = {
                     "Content-Type": "application/json",
@@ -35,26 +42,44 @@ class AliyunSignIn(object):
                 }
                 data = {}
                 res = requests.post(url, headers=headers, json=data)
-                if res.status_code == 200 and res.json()['success']:
-                    res_json = res.json()
-                    notice = ''
-                    prefix = ''
-                    for l in res_json['result']['signInLogs']:
-                        if l['status'] != 'miss':
-                            prefix = f'第{l["day"]}天'
-                            notice = l['notice'] or ''
-                            if l['reward'] and l['reward']['description']:
-                                notice += ' ' + l['reward']['description']
-                    notifyStr = f'{prefix}签到成功'
-                    if notice:
-                        notifyStr += f',获得【{notice}】'
-                    print(notifyStr)
-                else:
-                    print(f'获取access_token失败, refresh_token: {refresh_token}')
+                k = 1
+                if k < 10:
+                    print("k=", k)
+                    if res.status_code == 200 and res.json()['success']:
+                        k = 10
+                        res_json = res.json()
+                        notice = ''
+                        prefix = ''
+                        for l in res_json['result']['signInLogs']:
+                            if l['status'] != 'miss':
+                                prefix = f'第{l["day"]}天'
+                                notice = l['notice'] or ''
+                                if l['reward'] and l['reward']['description']:
+                                    notice += ' ' + l['reward']['description']
+                        notifyStr = f'{prefix}签到成功'
+                        if notice:
+                            notifyStr += f',获得【{notice}】'
+                        print(notifyStr)
+                        messages = messages + '\n' + notifyStr
+                        # notify.go_cqhttp("阿里云签到", notifyStr)
+                    else:
+                        k = k + 1
+                        print("kn=", k)
+                        print(f'获取access_token失败2, refresh_token: {refresh_token}')
+                        messages = messages + '\n' + f'获取access_token失败2, refresh_token: {refresh_token}'
+                        # notify.go_cqhttp("阿里云签到", '获取access_token失败2')
+
             else:
-                print(f'获取access_token失败, refresh_token: {refresh_token}')
+                print(f'获取access_token失败1, refresh_token: {refresh_token}')
+                messages = messages + '\n' + f'获取access_token失败1, refresh_token: {refresh_token}'
+                # notify.go_cqhttp("阿里云签到", '获取access_token失败1')
+        notify.send('阿里云盘签到', messages + '\n')
+
 
 if __name__ == '__main__':
-    refresh_tokens = ['token1', 'token2']
+    refresh_tokens = []
+    refresh_tokens.append(os.environ.get('alytoken'))
+    print('账号:', refresh_tokens, type(refresh_tokens))
     ali = AliyunSignIn(refresh_tokens)
     ali.sign_in()
+
--- miui.py
+++ miui.py
@@ -6,9 +6,9 @@ import os
 import urllib3
 import http.cookies
 import re
-​
+
 urllib3.disable_warnings()
-​
+
 credentials = os.environ.get('miui')
 if credentials:
   for credential in credentials.split('#'):
@@ -19,31 +19,31 @@ headers = {
     'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
     'Host': 'miuiver.com',
 }
-​
+
 data = {
     'log': username,
     'pwd': userpass,
     'action': 'mobantu_login',
 }
-​
+
 
 response = requests.post('https://miuiver.com/wp-content/plugins/erphplogin//action/login.php', headers=headers, data=data, verify=False)
-​
+
 cookie_value = re.findall(r'wordpress_logged_in_\w+[^;]+', response.headers['Set-Cookie'])[0]
-​
+
 cookies = {'wordpress_logged_in': cookie_value}
-​
+
 headers2 = {
     'content-type': 'application/x-www-form-urlencoded; charset=UTF-8', 
     'cookie': cookie_value ,
     'Host': 'miuiver.com',
 }
-​
+
 data2 = {
     'action': 'epd_checkin',
 }
 response2 = requests.post('https://miuiver.com/wp-admin/admin-ajax.php', headers=headers2, data=data2, verify=False)
-​
+
 if response2.status_code == 200:
     result = response2.json()
     if result['status'] == 201:
